7dd769c3ea726b189b5562f9b243d54caf7b8cdb
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-11-29 16:22:35 +0200

    feat(setting,signup screens): adding settings and signup screens with resources

--- app/src/main/java/com/example/vivochat/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/vivochat/presentation/ui/theme/Theme.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import com.example.vivochat.R
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -31,6 +34,20 @@ private val LightColorScheme = lightColorScheme(
     onSurface = Color(0xFF1C1B1F),
     */
 )
+val interFont = FontFamily(
+    Font(R.font.inter)
+)
+val sansFont = FontFamily(
+    Font(R.font.plusjakartasans)
+)
+val kumbuhFont = FontFamily(
+    Font(R.font.kumbuhsans)
+)
+val montserratFont = FontFamily(
+    Font(R.font.montserrat)
+)
+
+
 
 @Composable
 fun VivoChatTheme(
