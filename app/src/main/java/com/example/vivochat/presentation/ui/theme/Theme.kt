9536826f1f56ae8b104b9029378ded7921c42175
Yussifwafyy <yussifwafyy14@gmail.com>
2025-11-29 16:10:43 +0200

    Settings and sign up screens , fonts

--- app/src/main/java/com/example/vivochat/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/example/vivochat/presentation/ui/theme/Theme.kt
@@ -3,6 +3,7 @@ package com.example.vivochat.presentation.ui.theme
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -11,7 +12,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.example.vivochat.R
+import java.time.format.TextStyle
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -71,4 +75,13 @@ fun VivoChatTheme(
         typography = Typography,
         content = content
     )
+    val AppTypography = Typography(
+        bodyLarge =
+            androidx.compose.ui.text.TextStyle(
+                fontFamily = interFont,
+                fontWeight = FontWeight.Normal,
+                fontSize = 16.sp
+            )
+    )
+
 }
