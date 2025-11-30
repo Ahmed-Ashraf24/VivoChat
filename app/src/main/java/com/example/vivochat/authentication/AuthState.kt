4ef2d96c34beb2be41c8188e598e5de610a980d6
Yussifwafyy <yussifwafyy14@gmail.com>
2025-11-30 14:05:13 +0200

    Sign up and login Auth done

---
+++ app/src/main/java/com/example/vivochat/authentication/AuthState.kt
@@ -0,0 +1,9 @@
+package com.example.vivochat.authentication
+
+
+sealed class AuthState {
+    object Idle : AuthState()
+    object Loading : AuthState()
+    data class Success(val userId: String) : AuthState()
+    data class Error(val message: String) : AuthState()
+}
