df6d1bba2c46a699ce98ee2fffa31f3f334239f2
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-12-02 15:51:53 +0200

    refactor: refactor the firebase datasource , user repo and adding mapper

---
+++ app/src/main/java/com/example/vivochat/data/mappers/Mappers.kt
@@ -0,0 +1,13 @@
+package com.example.vivochat.data.mappers
+
+import com.example.vivochat.data.dto.UserDto
+import com.example.vivochat.domain.entity.User
+
+fun UserDto.toUser(): User {
+    return User(
+        this.userId,
+        this.fullName,
+        this.email,
+        this.phoneNum
+    )
+}
