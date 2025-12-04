f02ec3973774e94ea81d04ebe74a39ea7097fb42
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-12-05 01:23:22 +0200

    feat:adding filter contact

--- app/src/main/java/com/example/vivochat/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/vivochat/data/repository/UserRepository.kt
@@ -68,7 +68,7 @@ class UserRepository @Inject constructor(
 
             var userFound = false
             for (j in allUsers) {
-                if (i.phoneNum == j.phoneNum||i.phoneNum =="+20${j.phoneNum}") {
+                if (i.phoneNum == j.phoneNum||i.phoneNum =="+2${j.phoneNum}") {
 
                     availableContacts.add(j)
                     userFound = true
