62605582ed30aa90323bdb9ab107417dfabfa3e7
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-12-05 18:54:03 +0200

    refactor:removing unused models

--- app/src/main/java/com/example/vivochat/domain/entity/Response.kt
+++
@@ -1,8 +0,0 @@
-package com.example.vivochat.domain.entity
-
-sealed class Response {
-    data class Failed(val message:String): Response()
-    data class Success<T>(val result:T): Response()
-    object Loading : Response()
-
- }
--- app/src/main/java/com/example/vivochat/domain/entity/UserStories.kt
+++
@@ -1,9 +0,0 @@
-package com.example.vivochat.domain.entity
-
-data class UserStories(
-    val userId: String,
-    val username: String,
-    val profilePicUrl: String,
-    val stories: List<Story>,
-    var currentIndex: Int = 0
-)
